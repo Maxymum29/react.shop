6fe59e406961f844083055dc2b83c2ec04370681
Max <alimax@yandex.ru>
2021-12-21 15:10:28 +0300

    repo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,12 +2,17 @@ function Header() {
     return (
         <nav className=" green lighten-1">
             <div className="nav-wrapper">
-                <a href="#" className="brand-logo">
+                <a
+                    href="https://maxymum29.github.io/react.shop/"
+                    className="brand-logo"
+                >
                     React shop
                 </a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li>
-                        <a href="!#">Repo</a>
+                        <a href="https://github.com/Maxymum29/react.shop">
+                            Repo
+                        </a>
                     </li>
                 </ul>
             </div>
